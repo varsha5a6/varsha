ed48473417af8af7bf63073c0b0e5eb864036a53
varsha5a6 <32008048+varsha5a6@users.noreply.github.com>
2017-09-16 12:40:02 +0530

    Add files via upload

---
+++ student-array/src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		public static void main(String[] args) {
+
+		StudentGroup stugp=new StudentGroup(20);
+
+	Date d=new Date(1997,8,10);
+	Student s1=new Student(20,"varsha",d,70.0);
+
+                stugp.addFirst(s1);
+
+              System.out.println(stugp.getStudent(0).getId());
+
+	}
+
+}
